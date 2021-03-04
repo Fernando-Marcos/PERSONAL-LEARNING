ef9ca55356db854cb55828f0190c5297f7d59a97
Fernando Marcos <71319233+Fernando-Marcos@users.noreply.github.com>
2021-03-04 22:34:55 +0100

    'use strict'

--- Master JavaScript/JS/02-variables.js
+++ Master JavaScript/JS/02-variables.js
@@ -1,9 +1,14 @@
+'use strict'
 // VARIABLES
 // Una variable es un contenedor de información
 
 var pais = "España";
 var continente = "Europa";
 var antiguedad = 2021;
+
+
+pais = "Mexico";
+continente = "Latinoamerica";
 var pais_y_continente = pais+ ' ' +continente;
 
 console.log(pais, continente, antiguedad);
