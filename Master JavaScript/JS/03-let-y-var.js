cfb800d251bef376531c1726a2585f8ace46a600
Fernando Marcos <71319233+Fernando-Marcos@users.noreply.github.com>
2021-03-07 19:28:11 +0100

    // Pruebas con Let y var

--- Master JavaScript/JS/03-let-y-var.js
+++ Master JavaScript/JS/03-let-y-var.js
@@ -1,2 +1,26 @@
 'use strict'
+
 // Pruebas con Let y var
+
+// PRUEBA CON VAR
+var numero = 40;
+console.log(numero); // valor 40
+
+if (true) {
+  var numero = 50;
+  console.log(numero); // valor 50
+}
+
+console.log(numero); // valor 50
+
+
+// PRUEBA CON LET
+var texto = "Estoy aprendiendo JavaScript";
+console.log(texto); // Valor JS
+
+if (true) {
+  let texto = "Curso de Laravel 5"
+  console.log(texto); // Valor Laravel
+}
+
+console.log(texto); // Valor JS
