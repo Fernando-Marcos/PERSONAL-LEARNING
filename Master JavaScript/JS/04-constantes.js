a35c043ed6b02e98238754dc15e0ede034a801b0
Fernando Marcos <71319233+Fernando-Marcos@users.noreply.github.com>
2021-03-08 20:53:59 +0100

    Constantes

---
+++ Master JavaScript/JS/04-constantes.js
@@ -0,0 +1,12 @@
+"use strict"
+
+// CONSTANTES
+// Como una variable pero su valor no puede cambiar
+
+
+var web = "https://fernandomarcos.com";
+const ip = "192.168.0.12";
+
+web = "https://fernandomarcosdeveloper.com";
+
+console.log(web, ip);
